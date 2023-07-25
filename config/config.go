e1a3a56cf126146fc520cf790eb6bf81f7f74cb9
meizikeai <meizikeai@163.com>
2023-07-25 15:52:52 +0800

    update

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -24,6 +25,13 @@ func isProduction() bool {
 	if mode == "release" {
 		result = true
 	}
+
+	return result
+}
+
+func getKey(k string) string {
+	mode := getMode()
+	result := fmt.Sprintf("%s-%s", k, mode)
 
 	return result
 }
